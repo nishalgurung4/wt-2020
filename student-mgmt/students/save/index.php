77d94df66f903870a977ae6c0f980f6c8cc8a428
Nishal Gurung <nishal.gurung4@outlook.com>
2022-06-26 09:09:26 +0545

    insert data

--- student-mgmt/students/save/index.php
+++ student-mgmt/students/save/index.php
@@ -2,18 +2,21 @@
 
 require_once "../../utils/db.php";
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-$dob = $_POST['dob'];
-$favorite_color = $_POST['color'];
-$weight = $_POST['weight'];
-$gender = $_POST['gender'];
-$hobbies = implode(",", $_POST['hobbies']);
-$nationality = $_POST['nationality'];
+$name = $_POST['name'] ?? '';
+$email = $_POST['email'] ?? '';
+$password = password_hash($_POST['password'] ?? '', PASSWORD_DEFAULT);
+$dob = $_POST['dob'] ?? '';
+$favorite_color = $_POST['color'] ?? '';
+$weight = $_POST['weight'] ?? '';
+$gender = $_POST['gender'] ?? '';
+$hobbies = implode(",", $_POST['hobbies'] ?? []);
+$nationality = $_POST['nationality'] ?? 'NP';
 
 $sql = "INSERT INTO students (name, email, password, dob, favorite_color, weight, gender, hobbies, nationality)
 VALUES ('$name', '$email', '$password', '$dob', '$favorite_color', $weight, '$gender', '$hobbies', '$nationality')";
 
-
-die($sql);
+if($conn->query($sql) == TRUE) {
+	die("success");
+} else {
+	die("error");
+}
