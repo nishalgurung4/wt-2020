10ce3afc5327cfca1576c98b9bdba494a92f4755
Nishal Gurung <nishal.gurung4@outlook.com>
2022-06-26 08:55:31 +0545

    wip: crud example

---
+++ student-mgmt/students/save/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+require_once "../../utils/db.php";
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+$dob = $_POST['dob'];
+$favorite_color = $_POST['color'];
+$weight = $_POST['weight'];
+$gender = $_POST['gender'];
+$hobbies = implode(",", $_POST['hobbies']);
+$nationality = $_POST['nationality'];
+
+$sql = "INSERT INTO students (name, email, password, dob, favorite_color, weight, gender, hobbies, nationality)
+VALUES ('$name', '$email', '$password', '$dob', '$favorite_color', $weight, '$gender', '$hobbies', '$nationality')";
+
+
+die($sql);
---
+++ student-mgmt/utils/db.php
@@ -0,0 +1,12 @@
+<?php 
+
+$serverName = "localhost";
+$userName = "root";
+$password = "";
+$databaseName = "studentmgmt";
+
+$conn = new mysqli($serverName, $userName, $password, $databaseName);
+
+if($conn->connect_error) {
+	die("Connection failed!");
+}
